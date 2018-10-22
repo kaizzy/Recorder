1c14aca6d7158b2b4595b7f56d7a53d43ca2e63a
kaizzy <kaizzy@gmail.com>
2018-10-22 22:01:00 +0900

    1022-01

--- Recorder5/Form1.Designer.cs
+++ Recorder5/Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_refresh = new System.Windows.Forms.Button();
+            this.listview_sources = new System.Windows.Forms.ListView();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.btn_Upload = new System.Windows.Forms.Button();
+            this.btn_toText = new System.Windows.Forms.Button();
+            this.btn_translat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_refresh
+            // 
+            this.button_refresh.Location = new System.Drawing.Point(292, 43);
+            this.button_refresh.Name = "button_refresh";
+            this.button_refresh.Size = new System.Drawing.Size(237, 23);
+            this.button_refresh.TabIndex = 0;
+            this.button_refresh.Text = "Reflesh_Source";
+            this.button_refresh.UseVisualStyleBackColor = true;
+            this.button_refresh.Click += new System.EventHandler(this.button_refresh_Click);
+            // 
+            // listview_sources
+            // 
+            this.listview_sources.Location = new System.Drawing.Point(31, 43);
+            this.listview_sources.Name = "listview_sources";
+            this.listview_sources.Size = new System.Drawing.Size(219, 96);
+            this.listview_sources.TabIndex = 1;
+            this.listview_sources.UseCompatibleStateImageBehavior = false;
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(292, 87);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(99, 23);
+            this.button_Start.TabIndex = 2;
+            this.button_Start.Text = "Rec Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(425, 87);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(111, 23);
+            this.button_stop.TabIndex = 3;
+            this.button_stop.Text = "Rec Stop";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // btn_Upload
+            // 
+            this.btn_Upload.Location = new System.Drawing.Point(292, 129);
+            this.btn_Upload.Name = "btn_Upload";
+            this.btn_Upload.Size = new System.Drawing.Size(244, 23);
+            this.btn_Upload.TabIndex = 4;
+            this.btn_Upload.Text = "Google Storageへのアップロード";
+            this.btn_Upload.UseVisualStyleBackColor = true;
+            this.btn_Upload.Click += new System.EventHandler(this.btn_Upload_Click);
+            // 
+            // btn_toText
+            // 
+            this.btn_toText.Location = new System.Drawing.Point(292, 168);
+            this.btn_toText.Name = "btn_toText";
+            this.btn_toText.Size = new System.Drawing.Size(244, 23);
+            this.btn_toText.TabIndex = 5;
+            this.btn_toText.Text = "テキスト変換";
+            this.btn_toText.UseVisualStyleBackColor = true;
+            this.btn_toText.Click += new System.EventHandler(this.btn_toText_Click);
+            // 
+            // btn_translat
+            // 
+            this.btn_translat.Location = new System.Drawing.Point(292, 206);
+            this.btn_translat.Name = "btn_translat";
+            this.btn_translat.Size = new System.Drawing.Size(244, 23);
+            this.btn_translat.TabIndex = 6;
+            this.btn_translat.Text = "英語変換";
+            this.btn_translat.UseVisualStyleBackColor = true;
+            this.btn_translat.Click += new System.EventHandler(this.btn_translat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_translat);
+            this.Controls.Add(this.btn_toText);
+            this.Controls.Add(this.btn_Upload);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.listview_sources);
+            this.Controls.Add(this.button_refresh);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_refresh;
+        private System.Windows.Forms.ListView listview_sources;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.Button btn_Upload;
+        private System.Windows.Forms.Button btn_toText;
+        private System.Windows.Forms.Button btn_translat;
     }
 }
 
--- Recorder5/Form1.cs
+++ Recorder5/Form1.cs
@@ -8,6 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using NAudio.Wave;
+using System.IO;
+using Google.Cloud.Translation.V2;
+using Google.Cloud.Speech.V1;
+using Google.Cloud.Storage.V1;
+
+
 namespace Recorder5
 {
     public partial class Form1 : Form
@@ -15,6 +22,209 @@ namespace Recorder5
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button_refresh_Click(object sender, EventArgs e)
+        {
+                        // setup WaveIn Devices
+            //souce をクラス化：WaveInCapabilities
+            List<WaveInCapabilities> sources = new List<WaveInCapabilities>();
+            
+            //sources に 利用できるデバイスをDeviceCount 数分 追加する。
+            for (int i = 0; i < WaveIn.DeviceCount; i++)
+            {
+                sources.Add(WaveIn.GetCapabilities(i));
+            }
+            
+            listview_sources.Items.Clear();
+            
+            //listview_sourc にitem を表示する。
+
+            foreach (var source in sources)
+            {
+                ListViewItem item = new ListViewItem(source.ProductName);
+                item.SubItems.Add(new ListViewItem.ListViewSubItem(item, source.Channels.ToString()));
+
+                listview_sources.Items.Add(item);
+            }
+        }
+
+
+        #region Member
+
+        private NAudio.Wave.WaveIn sourceStream = null;
+        private NAudio.Wave.DirectSoundOut waveOut = null;
+        private NAudio.Wave.WaveFileWriter waveWriter = null;
+
+
+
+
+        #endregion
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+
+            //マイク元を指定していない場合。
+            if (listview_sources.SelectedItems.Count == 0) return;
+
+            //オーディオチェーン:WaveIn(rec)  ⇒ Callback() ⇒ waveWriter
+
+            //録音先のwavファイル
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Wave File (*.wav)|*.wav;";
+            if (save.ShowDialog() != DialogResult.OK) return;
+
+            //選択した録音デバイス番号
+            int deviceNumber = listview_sources.SelectedItems[0].Index;
+
+            //waveIn selet Recording Deivce
+
+            sourceStream = new WaveIn();　　//sourceStreamは、78で定義
+            sourceStream.DeviceNumber = deviceNumber;
+            //  sourceStream.WaveFormat = new WaveFormat(16000, WaveIn.GetCapabilities(deviceNumber).Channels);
+            sourceStream.WaveFormat = new WaveFormat(16000, 1);
+
+            //録音のコールバックkな数 k??
+            sourceStream.DataAvailable += new EventHandler<WaveInEventArgs>(sourceStream_DataAvailable);
+
+            //wave 出力
+            waveWriter = new WaveFileWriter(save.FileName, sourceStream.WaveFormat);
+
+            //録音開始
+            sourceStream.StartRecording();
+
+        }
+
+        private void sourceStream_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveWriter == null) return;
+
+            waveWriter.Write(e.Buffer, 0, e.BytesRecorded);
+            waveWriter.Flush();
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            waveOut?.Stop();
+            waveOut?.Dispose();
+            waveOut = null;
+
+            sourceStream?.StopRecording();
+            sourceStream?.Dispose();
+            sourceStream = null;
+
+            waveWriter?.Dispose();
+            waveWriter = null;
+
+
+
+        }
+
+        private void btn_Upload_Click(object sender, EventArgs e)
+        {
+
+            //https://qiita.com/shiki_hskw/items/b8f365d6b8075bf4c29b
+            //https://leadtools.grapecity.com/topics/news-20170726
+            //https://cloud.google.com/storage/docs/object-basics?hl=ja#storage-download-object-python
+
+            string bucketName = "kh1009";
+            string localPath = @"D:\Temp\";
+            string fileName = "translate.wav";
+
+
+
+            try
+            {
+                var storage = StorageClient.Create();
+                using (var f = File.OpenRead(localPath + "/" + fileName))
+                {
+                    //objectName = objectName ?? Path.GetFileName(localPath);
+                    storage.UploadObject(bucketName, fileName, null, f);
+                    MessageBox.Show($"Uploaded {fileName}.");
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+                //Console.WriteLine(exc.Message);
+                return;
+            }
+        }
+
+        static object AsyncRecognize(string storageUri)
+        {
+            var speech = SpeechClient.Create();
+            DateTime Start_time;
+
+            Start_time = DateTime.Now;
+
+            var longOperation = speech.LongRunningRecognize(
+                new RecognitionConfig()
+                {
+                    Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                    SampleRateHertz = 16000,
+                    LanguageCode = "ja-JP",
+                },
+                RecognitionAudio.FromStorageUri(storageUri));
+
+            longOperation = longOperation.PollUntilCompleted();
+            var response = longOperation.Result;
+
+            foreach (var result in response.Results)
+            {
+                foreach (var alternatime in result.Alternatives)
+                {
+                    File.AppendAllText(@"D:\temp\transcript.txt", alternatime.Transcript + Environment.NewLine);                    
+                }
+            }
+            MessageBox.Show("開始:" + Start_time + "  終了：" + DateTime.Now + "完了");
+            return 0;
+        }
+        private void btn_toText_Click(object sender, EventArgs e)
+        {
+            //The path should be gs://<bucket_name>/<file_path_inside_bucket>.  
+            string storageUri = "gs://kh1009/translate.wav";
+
+            //Console.WriteLine(storageUri);
+            //File_Name.Text = "storage URI: " + storageUri;
+
+            //System.Threading.Thread.Sleep(50);
+            
+            AsyncRecognize(storageUri);
+
+            //Console.ReadKey();
+
+
+        }
+
+        private void btn_translat_Click(object sender, EventArgs e)
+        {
+            string s;
+            string text = "";
+            FileStream fin;
+
+            fin = new FileStream(@"D:\Temp\transcript.txt", FileMode.Open);
+            StreamReader fstr_in = new StreamReader(fin);
+
+            while ((s = fstr_in.ReadLine()) != null)
+            {
+                text = text + s;
+            }
+            //string text = @"D:\Temp\transcript.txt";
+            fstr_in.Close();
+
+
+            string targetLanguageCode = "en";
+            string sourceLanguageCode = "ja";
+
+
+            TranslationClient client = TranslationClient.Create();
+            var response = client.TranslateText(text, targetLanguageCode, sourceLanguageCode);
+
+            File.AppendAllText(@"D:\temp\English_txt.txt", Convert.ToString(response));
+            //Console.WriteLine(resonse.TranslatedText);
+            //Console.ReadKey();
+
         }
     }
 }
